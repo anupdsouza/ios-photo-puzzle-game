3fbb471d7ae22249ca99a32bb22b9306cbaa9553
Anup DSouza <anup.dsouza@gmail.com>
2024-04-02 15:22:05 +0530

    Fetch & display image

--- PhotoPuzzle/PhotoPuzzle/ContentView.swift
+++ PhotoPuzzle/PhotoPuzzle/ContentView.swift
@@ -6,16 +6,42 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var selectedImage: UIImage?
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            if let image = selectedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            }
+
+            PhotosPicker(selection: $selectedItem, matching: .images) {
+                Image(systemName: "photo.stack.fill")
+                    .frame(width: 40, height: 25)
+            }
+            .onChange(of: selectedItem, loadImage)
         }
         .padding()
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            selectedImage = inputImage
+        }
+    }
+    
+    func processImage() {
+        if let image = selectedImage {
+            let size = min(image.size.width, image.size.height)
+            // TODO: Clip portion of the image from the center as a square of the given size
+        }
     }
 }
 
