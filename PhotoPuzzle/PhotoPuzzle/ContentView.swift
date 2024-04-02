b4daebd3f4302bb71f1e4b82215790e56afcfe07
Anup DSouza <anup.dsouza@gmail.com>
2024-04-02 12:25:37 +0530

    Add initial files

---
+++ PhotoPuzzle/PhotoPuzzle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoPuzzle
+//
+//  Created by Anup D'Souza on 02/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoPuzzle/PhotoPuzzle/PhotoPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPuzzleApp.swift
+//  PhotoPuzzle
+//
+//  Created by Anup D'Souza on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
